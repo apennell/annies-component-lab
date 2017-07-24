1e4e3afdd97fdf999bbdf5fd3e40401cb36eb7ee
Annie Pennell <anniecpennell@gmail.com>
2017-07-24 15:12:54 -0700

    added button component code, style, and documentation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
+
+import Button from './components/Elements/Button/Button.jsx';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1>Annie's Guide to Great Style and Components in React!</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className={styles.intro}>
+          This is my styleguide!
         </p>
+        <div>
+          <Button 
+            buttonStyle="primary" 
+            size="large"
+            onClick={(e) => alert('You clicked a unique button!', e.target)}
+          >
+            Working Button!
+          </Button>
+        </div>
       </div>
     );
   }
